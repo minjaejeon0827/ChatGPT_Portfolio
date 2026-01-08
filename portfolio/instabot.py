5319deda7266d547bc567d58811f82094754872b
DESKTOP-5NUN1M7\bhjeon <bhjeon@imbu.co.kr>
2026-01-08 09:42:01 +0900

    instabot.py - 인스타그램 포스팅 생성기 프로그램 4차 커밋

--- portfolio/instabot.py
+++ portfolio/instabot.py
@@ -1,18 +1,11 @@
 """
-*** 참고 ***
-*** ChatGPT 문서 ***
-* ChatGPT 텍스트 응답 메시지
-참고 URL - https://github.com/openai/openai-python
+* 인스타그램 포스팅 생성기 프로그램
 
+*** 참고 ***
 *** 파이썬 문서 ***
 * with 문
 참고 URL - https://docs.python.org/ko/3/reference/compound_stmts.html#index-16
 참고 2 URL - https://velog.io/@hyungraelee/Python-with
-
-* urllib
-참고 URL - https://docs.python.org/ko/3.13/library/urllib.html
-참고 2 URL - https://docs.python.org/ko/3.13/library/urllib.request.html#module-urllib.request
-참고 3 URL - https://docs.python.org/ko/3.13/library/urllib.request.html#legacy-interface
 
 * PIL.Image: 이미지 처리 모듈
 참고 URL - https://pillow.readthedocs.io/en/latest/reference/Image.html#
@@ -23,9 +16,6 @@
 참고 URL - https://github.com/adw0rd/instagrapi
 참고 2 URL - https://github.com/subzeroid/instagrapi
 
-* 구글 번역기 오픈 소스 패키지 googletrans
-참고 URL - https://pypi.org/project/googletrans/
-
 * RGB (빛의 3원색 - 빨강, 초록, 파랑)
 참고 URL - https://ko.wikipedia.org/wiki/RGB
 """
@@ -35,33 +25,14 @@
 # streamlit run instabot.py
 
 ##### 패키지 불러오기 #####
-import streamlit as st   # streamlit -> Elias(앨리아스) st
-import openai   # OpenAI (ChatGPT, DALLE2 사용 목적)
-import urllib   # URL 처리
+from utils import openAI_util   # OpenAI 전용 유틸(util)
 
-from googletrans import Translator   # 구글 번역기
+import streamlit as st   # streamlit -> Elias(앨리아스) st
+
 from instagrapi import Client   # Instagram
 from PIL import Image   # 이미지 처리
 
 ##### 기능 구현 함수 #####
-def google_trans(prompt: str) -> str:
-    """
-    Description: 한글 -> 영어 번역 (DALLE2 이미지 생성 용도)
-
-                 현재 DALLE2에서 한글 텍스트 사용시 이미지가 엉망진창으로 생성됨.
-                 왜냐면 아직 한글에 대한 학습이 원활하지 않는 것으로 확인.
-                 하여 한글 -> 영어 번역한 텍스트 사용해서 DALLE2 이미지 생성함.
-
-    Parameters: prompt - 사용자 작성 내용 (주제, 분위기)
-
-    Returns: result.text - 한글 -> 영어 번역 내용
-    """
-
-    google = Translator()
-    result = google.translate(prompt, dest="en")   # 한글 -> 영어 번역 (dest="en") 참고: 영어 -> 한글 번역 (dest="ko")
-
-    return result.text   # 한글 -> 영어 번역 내용 리턴
-
 # TODO: upload_instagram 함수 호출시 아래 오류 메시지 출력 되어 추후 필요시 해당 오류 보완 예정 (2026.01.07 minjae)
 # 오류 메시지 - "BadPassword: We can send you an email to help you get back into your account. 
 #              If you are sure that the password is correct, then change your IP address, 
@@ -80,64 +51,6 @@ def google_trans(prompt: str) -> str:
 #     client = Client()
 #     client.login(st.session_state["instagram_ID"], st.session_state["instagram_Password"])   # 인스타그램 로그인 처리
 #     client.photo_upload("insta_image_resize.jpg" , description)   # 인스타그램 사진 이미지("insta_image_resize.jpg") 및 포스팅 글(description) 업로드
-
-def get_descriptionFromGPT(topic: str, mood: str) -> str:
-    """
-    Description: ChatGPT 텍스트 응답 메시지 가져오기 (인스타그램 포스팅 글)
-
-    Parameters: topic - 주제
-                mood - 분위기
-
-    Returns: msg - ChatGPT 텍스트 응답 메시지 (인스타그램 포스팅 글)
-    """
-
-    # f'''~~~~~''' - 시스템 프롬프트와 매개변수 topic, mood 합쳐서 구현.
-    # 시스템 프롬프트 문자열
-    # 1. 이모지 포함(Include emojis)
-    # 2. 이목을 끌말한 내용으로 첫번째 caption 문장 작성 (The first caption sentence should hook the readers.)
-    # 3. 모든 것은 한국어로 작성 (write all output in korean.)
-    prompt = f'''
-            Write me the Instagram post description or caption in just a few sentences for the post 
-            -topic : {topic}
-            -Mood : {mood}
-            Format every new sentence with new lines so the text is more readable.
-            Include emojis and the best Instagram hashtags for that post.
-            The first caption sentence should hook the readers.
-            write all output in korean.
-            '''
-    
-    messages_prompt = [{"role": "system", "content": prompt}]
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages_prompt)
-
-    msg = response["choices"][0]["message"]["content"]   # ChatGPT 텍스트 응답 메시지 msg 변수 저장
-    return msg
-
-def get_downLoad_imageFromDALLE2(topic: str, mood: str) -> None:
-    """
-    Description: DALLE.2 이미지 생성 및 다운로드
-
-    Parameters: topic - 주제
-                mood - 분위기
-
-    Returns: 없음.
-    """
-
-    # 한글 -> 영어 번역
-    t_topic = google_trans(topic)   # 주제
-    t_mood = google_trans(mood)     # 분위기
-    
-
-    # f'~~~~~' - 시스템 프롬프트와 t_topic, t_mood 변수 합쳐서 구현.
-    # 시스템 프롬프트 문자열
-    # 1. 해당 주제에 대해서 그림 그리기 (Draw picture about {t_topic})
-    # 2. 그림의 분위기는 다음과 같다. (picture Mood is {t_mood}')
-    image_prompt = f'Draw picture about {t_topic}. picture Mood is {t_mood}'
-    print(image_prompt)
-
-    response = openai.Image.create(prompt=image_prompt, n=1, size="512x512")   # 이미지 생성 및 response 변수 저장
-
-    image_url = response['data'][0]['url']  # 이미지 URL 주소(response['data'][0]['url']) image_url 변수 저장
-    urllib.request.urlretrieve(image_url, "insta_image.jpg")  # 해당 이미지 다운로드 및 "insta_image.jpg" 파일명 저장
 
 ##### 메인 함수 #####
 def main() -> None:
@@ -191,7 +104,7 @@ def main() -> None:
         # 참고 3 URL - https://jino-dev-diary.tistory.com/42
         # 참고 4 URL - https://claude.ai/chat/eaf7856e-1b5e-4c26-992e-de1683005638
         if open_api_key:   # open_api_key 변수 할당된 값이 None 또는 공백("")이 아닌 경우 (None or Empty String Check)
-            openai.api_key = open_api_key   # openai.api_key 변수에 입력 받은 open_api_key 값을 저장 (이렇게 처음에 OpenAI API 키 지정 한번 해 놓으면 OpenAI 패키지를 사용하는 코드 안에서는 더이상 따로 API 입력할 필요 없음.)
+            openAI_util.openai.api_key = open_api_key   # openAI_util.openai.api_key 변수에 입력 받은 open_api_key 값을 저장 (이렇게 처음에 OpenAI API 키 지정 한번 해 놓으면 OpenAI 패키지를 사용하는 코드 안에서는 더이상 따로 API 입력할 필요 없음.)
         
         st.markdown('---')   # 구분선 추가('---') - 혹시 밑에 다른 엘리멘트들을 추가할 때 대비해서 구현.
 
@@ -215,13 +128,30 @@ def main() -> None:
 
     if st.button(label="생성", type="secondary") and not st.session_state["flag"]:   # "생성" 버튼 화면 출력 및 해당 버튼 Click 이벤트 발생 시 st.session_state["flag"] == false 경우만 if 문 실행
         with st.spinner('생성 중'):   # 인스타그램 포스팅 '생성 중' 대기 처리 화면 출력 (포스팅 생성 완료 시 해당 대기 처리 화면 종료)
-            st.session_state["description"] = get_descriptionFromGPT(topic, mood)   # 인스타그램 포스팅 글 가져오기
-            get_downLoad_imageFromDALLE2(topic,mood)   # DALLE.2 이미지 생성 및 다운로드
+            # f'''~~~~~''' - 시스템 프롬프트와 매개변수 topic, mood 합쳐서 구현.
+            # 시스템 프롬프트 문자열
+            # 1. 이모지 포함(Include emojis)
+            # 2. 이목을 끌말한 내용으로 첫번째 caption 문장 작성 (The first caption sentence should hook the readers.)
+            # 3. 모든 것은 한국어로 작성 (write all output in korean.)
+            prompt = f'''
+                    Write me the Instagram post description or caption in just a few sentences for the post 
+                    -topic : {topic}
+                    -Mood : {mood}
+                    Format every new sentence with new lines so the text is more readable.
+                    Include emojis and the best Instagram hashtags for that post.
+                    The first caption sentence should hook the readers.
+                    write all output in korean.
+                    '''
+
+            messages_prompt = [{"role": "system", "content": prompt}]
+
+            st.session_state["description"] = openAI_util.get_response(messages_prompt)   # 인스타그램 포스팅 글 가져오기
+            openAI_util.downLoad_image_dalle2(topic,mood)   # DALLE.2 이미지 생성 및 다운로드
 
             st.session_state["flag"] = True   # 인스타그램 포스팅 생성 완료 ("업로드" 버튼 클릭해도 해당 포스팅 중복 생성 불가.)
 
     if True == st.session_state["flag"]:   # 인스타그램 포스팅 생성 완료된 경우
-        image = Image.open("insta_image.jpg")   # get_downLoad_imageFromDALLE2 함수 호출 결과 다운로드 받은 이미지 파일 열기 ("insta_image.jpg")
+        image = Image.open("insta_image.jpg")   # downLoad_image_dalle2 함수 호출 결과 다운로드 받은 이미지 파일 열기 ("insta_image.jpg")
         st.image(image)   # 해당 포스팅 생성 완료된 이미지 화면 출력 ("insta_image.jpg") 
 
         # 오류 메시지 "streamlit.errors.StreamlitAPIException: Invalid height 50px for st.text_area - must be at least 68 pixels." 출력으로 인하여
