9d81b86c6b2688f1f3b84acfce435924581155de
DESKTOP-5NUN1M7\bhjeon <bhjeon@imbu.co.kr>
2026-01-09 09:28:27 +0900

    openAI_util.py - OpenAI 전용 유틸 (util) 5차 커밋

--- portfolio/utils/openAI_util.py
+++ portfolio/utils/openAI_util.py
@@ -15,13 +15,14 @@
 
 ##### 패키지 불러오기 #####
 from utils import google_util   # Google 전용 유틸(util)
+from typing import Any          # Type Hints class Any
 
 import openai
 import urllib   # URL 처리
 
 ##### 기능 구현 함수 #####
 ##### 프로그램 내에서 ChatGPT한테 물어보거나 내용 번역 또는 요약 지시하거나 하는 그러한 기능들을 깔끔하게 함수화해서 정리
-def get_response(messages_prompt: list[dict]) -> str:
+def get_response(messages_prompt: Any) -> str:
     """
     Description: ChatGPT 텍스트 응답 메시지 가져오기
 
@@ -30,7 +31,7 @@ def get_response(messages_prompt: list[dict]) -> str:
     Returns: msg - ChatGPT 텍스트 응답 메시지
     """
 
-    response = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=messages_prompt)  # ChatGPT 응답 받기 및 response 변수 저장
+    response = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=messages_prompt)   # ChatGPT 응답 받기 및 response 변수 저장
 
     msg = response["choices"][0]["message"]["content"]   # ChatGPT 텍스트 응답 메시지 msg 변수 저장
     return msg
